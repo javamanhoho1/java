f1f62464429fe049c00878b90a858df7c6b906a1
javamanhoho1 <52848109+javamanhoho1@users.noreply.github.com>
2019-07-13 14:46:22 +0900

    Add files via upload

---
+++ 변수/Hi.java
@@ -0,0 +1,17 @@
+package 변수;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		//이름
+		//소속
+		//나이
+		System.out.println("두더지");
+		System.out.println("미국");
+		System.out.println("30세");
+		
+		
+		
+	}
+
+}
---
+++ 변수/House.java
@@ -0,0 +1,26 @@
+package 변수;
+
+public class House {
+
+	public static void main(String[] args) {
+		String color ="초록";
+		String number = 844+ "-" +1234;
+		int room = 302;
+		int floor = 10;
+		double window = 50.5;
+		char direction = '좌';
+		Boolean cool = true;
+		String info = "저의 집에 대한 정보입니다";
+		
+		System.out.println("집지붕색깔: " + color);
+		System.out.println("집전화번호: " + number );
+		System.out.println("호실: " + room);
+		System.out.println("층수: " + floor);
+		System.out.println("창문크기: " + window);
+		System.out.println("집 위치: " + direction);
+		System.out.println("집이 시원합니까: " + cool);
+		
+
+	}
+
+}
